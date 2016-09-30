3215466b58ad8f3c0a0143485b78cc79a59a2e3f
davidkolundzic <david_k42@hotmail.com>
2016-09-30 13:06:24 +0200

    prvi commit git

---
+++ app/app.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('contactManagerApp', [
+        // Angular modules
+        //'ngRoute'
+
+        // Custom modules
+
+        // 3rd Party Modules
+        'ngMaterial',
+
+        ])
+        .config(($mdIconProvider,$mdThemingProvider) => {
+            $mdIconProvider
+                .defaultIconSet('/Images/avatars.svg',128)
+                .iconSet("action", "/Content/angular-material-icons/action.svg")
+                .iconSet("alert", "/Content/angular-material-icons/alert.svg")
+                .iconSet("av", "/Content/angular-material-icons/av.svg")
+                .iconSet("communication", "/Content/angular-material-icons/communication.svg")
+                .iconSet("content", "/Content/angular-material-icons/content.svg")
+                .iconSet("device", "/Content/angular-material-icons/device.svg")
+                .iconSet("editor", "/Content/angular-material-icons/editor.svg")
+                .iconSet("file", "/Content/angular-material-icons/file.svg")
+                .iconSet("hardware", "/Content/angular-material-icons/hardware.svg")
+                .iconSet("image", "/Content/angular-material-icons/image.svg")
+                .iconSet("maps", "/Content/angular-material-icons/maps.svg")
+                .iconSet("navigation", "/Content/angular-material-icons/navigation.svg")
+                .iconSet("notification", "/Content/angular-material-icons/notification.svg")
+                .iconSet("soclal", "/Content/angular-material-icons/soclal.svg")
+                .iconSet("toggle", "/Content/angular-material-icons/toggle.svg");
+            $mdThemingProvider.theme('default')
+                //.dark();
+                .primaryPalette('indigo')
+                .accentPalette('red');
+        });
+})();
---
+++ app/controllers/mainController.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('contactManagerApp')
+        .controller('mainController', mainController);
+
+    mainController.$inject = ['userService','$mdSidenav'];
+
+    function mainController(userService, $mdSidenav) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'mainController';
+        vm.uers = [];
+        vm.selected = null;
+        vm.toggleSideNav = toggleSideNav;
+        vm.selectUser = selectUser;
+        vm.searchText = '';
+        vm.tabIndex = 0;
+
+
+        userService.loadAllUsers().then(
+            function (data) {
+                vm.users = data;
+                vm.selected = vm.users[0];
+            },
+            function myfunction() {
+
+            })
+
+        function toggleSideNav() {
+            $mdSidenav('left').toggle();
+            //$mdSidenav('left').toggle();
+        }
+        function selectUser(user) {
+            vm.selected = user;
+            var sidenav = $mdSidenav('left');
+            if (sidenav.isOpen) {
+                sidenav.close();
+
+            }
+            vm.tabIndex = 0;
+        }
+
+        activate();
+        function activate() { }
+    }
+})();
